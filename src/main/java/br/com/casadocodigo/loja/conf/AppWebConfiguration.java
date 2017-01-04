0dbb80e763227d525f1a5530ed4919ff127ea402
Josue Freitas <josue1912@gmail.com>
2017-01-04 16:13:15 -0200

    Retornando no formato json

--- src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -1,5 +1,7 @@
 package br.com.casadocodigo.loja.conf;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.cache.CacheManager;
@@ -13,12 +15,15 @@ import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.google.common.cache.CacheBuilder;
@@ -34,17 +39,30 @@ import br.com.casadocodigo.loja.models.CarrinhoCompras;
 public class AppWebConfiguration extends WebMvcConfigurerAdapter{
 
 	/**
+	 * Configurando um viewResolver para responder no formato JSON ou JSP
+	 */
+	@Bean
+	public ViewResolver contentNegotiationViewResolver(ContentNegotiationManager manager){
+		List<ViewResolver> viewResolvers = new ArrayList<>();
+		viewResolvers.add(internalResourceViewResolver());
+		viewResolvers.add(new JsonViewResolver());
+		
+		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+		resolver.setViewResolvers(viewResolvers);
+		resolver.setContentNegotiationManager(manager);		
+		return resolver;
+	}
+	
+	/**
 	 * Configurando o cache
 	 */
 	@Bean
 	public CacheManager cacheManager(){
-		// return new ConcurrentMapCacheManager();
 		CacheBuilder<Object, Object> builder = CacheBuilder.newBuilder().maximumSize(100).expireAfterAccess(5, TimeUnit.MINUTES);
 		GuavaCacheManager manager = new GuavaCacheManager();
 		manager.setCacheBuilder(builder);
 		return manager;
 	}
-	
 	
 	/**
 	 * Liberando a aplicação para responder aos recursos estáticos
@@ -55,7 +73,7 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter{
     }
 	
 	/**
-	 * Configurando a pasta onde se encontram as views e a extensão do arquivo 
+	 * Configurando um viewResolver para responder aos arquivos JSP 
 	 * @return resolver
 	 */
 	@Bean
--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -32,7 +32,7 @@ public class ProdutosController {
 
 	@Autowired
 	private FileSaver fileSaver;
-	
+
 	@InitBinder
 	public void InitBinder(WebDataBinder binder) {
 		binder.addValidators(new ProdutoValidation());
@@ -40,27 +40,28 @@ public class ProdutosController {
 
 	@RequestMapping("/form")
 	public ModelAndView form(Produto produto) {
-		
+
 		ModelAndView modelAndView = new ModelAndView("produtos/form");
 		modelAndView.addObject("tipos", TipoPreco.values());
-		
+
 		return modelAndView;
 	}
 
-	@RequestMapping(method = RequestMethod.POST, name="ProdutoGravar")
-	@CacheEvict(value="produtosHome", allEntries=true)
-	public ModelAndView gravar(MultipartFile sumario, @Valid Produto produto, BindingResult result, RedirectAttributes redirectAttributes) {
-		
+	@RequestMapping(method = RequestMethod.POST, name = "ProdutoGravar")
+	@CacheEvict(value = "produtosHome", allEntries = true)
+	public ModelAndView gravar(MultipartFile sumario, @Valid Produto produto, BindingResult result,
+			RedirectAttributes redirectAttributes) {
+
 		if (result.hasErrors()) {
 			return form(produto);
 		}
-		
+
 		String path = fileSaver.write("arquivos-sumario", sumario);
 		produto.setSumarioPath(path);
-		
+
 		dao.gravar(produto);
 		redirectAttributes.addFlashAttribute("message", "Produto cadastrado com sucesso!");
-		
+
 		return new ModelAndView("redirect:produtos");
 	}
 
@@ -72,11 +73,11 @@ public class ProdutosController {
 		return modelAndView;
 	}
 
-	@RequestMapping("/detalhe/{id}")
-	public ModelAndView detalhe(@PathVariable("id") Integer id){
+	@RequestMapping(value = "/detalhe/{id}", method = RequestMethod.GET)
+	public ModelAndView detalhe(@PathVariable("id") Integer id) {
 		ModelAndView modelAndView = new ModelAndView("produtos/detalhe");
 		Produto produto = dao.buscaPorId(id);
-		modelAndView.addObject("produto",produto);
+		modelAndView.addObject("produto", produto);
 		return modelAndView;
 	}
 }
